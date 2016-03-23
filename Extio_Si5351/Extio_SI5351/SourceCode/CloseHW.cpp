e69b38de6923d7152892bbb42cbd8aa6332dcad6
unknown <Owner@Owner-HP.(none)>
2016-03-22 20:26:16 -0400

    Initial commit

---
+++ Extio_Si5351/Extio_SI5351/SourceCode/CloseHW.cpp
@@ -0,0 +1,26 @@
+#include <windows.h> 
+#include "lusb0_usb.h"
+#include "extio_si5351.h" 
+#include "SI5351.h"
+
+extern long m_loFreq;
+extern usb_dev_handle *handle;
+
+/*  
+This entry is called when the user indicates that control of the external HW is 
+no longer needed or wanted. This is done by choosing ShowOptions | Select Input 
+then selecting either WAV file or Sound Card.  The  DLL  can  use  this  information 
+to  e.g.  shut  down  its  GUI  interface,  if  any,  and  possibly  to  put  the 
+controlled HW in a idle status. 
+*/
+void DLL CloseHW(void) 
+{
+	// Close the device if it's been opened.
+	if (handle != null)
+	{
+		SI5351_OutputDisable(0);
+		SI5351_OutputDisable(1);
+		usb_close(handle);
+		handle = null;
+	}
+}
---
+++ Extio_Si5351/Extio_SI5351/SourceCode/StartHW.cpp
@@ -0,0 +1,26 @@
+#include <windows.h> 
+#include "extio_si5351.h" 
+#include "SI5351.h"
+
+extern long m_loFreq;
+extern bool m_outputEnabled;
+
+/*
+This entry is called each time the user presses the Start button on the main screen
+after having previously specified that the DLL is in control of the external hardware. 
+*/
+int DLL StartHW(long freq) 
+{ 
+	// Set the frequency of the controlled hardware to LOfreq 
+	// Normally you would enable the output here but if you set m_outputEnabled
+	// to TRUE before setting the frequency it will be taken care of automatically.
+	m_outputEnabled = true;
+	SetHWLO(freq);
+	//SI5351_OutputEnable(0);
+	//SI5351_OutputEnable(1);
+
+
+	// Return the number of complex elements to be returned on each
+	// invocation of the callback routine.
+	return 0;
+} 
